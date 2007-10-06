b30483cf2c543fa056a29cd618450c76f3fc344e
cetoli <cetoli@87632e9f-ef39-0410-bef6-35b1ae510437>
2007-10-06 16:41:01 +0000

    mapas conceituais - carga

---
+++ trunk/neurolabase/Neuropedagogia.mm
@@ -0,0 +1,197 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1191431614110" ID="Freemind_Link_470030231" MODIFIED="1191490117361" TEXT="Neuropedagogia">
+<edge WIDTH="2"/>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1191431776383" ID="Freemind_Link_835263141" MODIFIED="1191448065919" POSITION="left" STYLE="bubble" TEXT="desenvolvimento">
+<edge COLOR="#ab610d"/>
+<node CREATED="1191433144205" ID="Freemind_Link_44704473" MODIFIED="1191433237335" TEXT="isomorfismo sociogen&#xe9;tico">
+<node CREATED="1191433248667" ID="Freemind_Link_669107674" MODIFIED="1191489982359" TEXT="arte rupestre">
+<edge COLOR="#ab0d33"/>
+</node>
+<node CREATED="1191433261935" ID="Freemind_Link_1336493866" MODIFIED="1191489996863" TEXT="cer&#xe2;mica">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191433289301" ID="Freemind_Link_1997223206" MODIFIED="1191489998591" TEXT="entalhamento">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191433303818" ID="Freemind_Link_8272648" MODIFIED="1191490000047" TEXT="pictograma">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191433419092" ID="Freemind_Link_1692785333" MODIFIED="1191490007963" TEXT="ideograma">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191433449722" ID="Freemind_Link_350290047" MODIFIED="1191490009051" TEXT="escrita">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191436113354" ID="Freemind_Link_391202273" MODIFIED="1191439339892" TEXT="neuropedagogia em campo">
+<node CREATED="1191439170196" ID="Freemind_Link_959453852" MODIFIED="1191490010435" TEXT="alfabetiza&#xe7;&#xe3;o meninos lobo">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191488041520" ID="Freemind_Link_1016888278" MODIFIED="1191490012075" TEXT="comunidades carentes">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191439204338" ID="Freemind_Link_352228220" MODIFIED="1191490013267" TEXT="vara de inf&#xe2;ncia">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191439293487" ID="Freemind_Link_1241074001" MODIFIED="1191490014639" TEXT="crian&#xe7;as abusadas">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191439344902" ID="Freemind_Link_1147917371" MODIFIED="1191490015859" TEXT="disfun&#xe7;&#xe3;o cerebral">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191439405445" ID="Freemind_Link_1050644765" MODIFIED="1191490016955" TEXT="observa&#xe7;&#xe3;o cl&#xed;nica">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+<node CREATED="1191488060996" ID="Freemind_Link_1268271140" MODIFIED="1191490017951" TEXT="ci&#xea;ncia experimental">
+<edge COLOR="#ab0d33" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191433238022" ID="Freemind_Link_1008056025" MODIFIED="1191433607628" TEXT="jogos educativos">
+<node CREATED="1191433769605" ID="Freemind_Link_1161609568" MODIFIED="1191434834664" TEXT="cogni&#xe7;&#xe3;o"/>
+<node CREATED="1191434835561" ID="Freemind_Link_1396125587" MODIFIED="1191434844759" TEXT="elabora&#xe7;&#xe3;o"/>
+<node CREATED="1191434845622" ID="Freemind_Link_860908640" MODIFIED="1191434884606" TEXT="metacogni&#xe7;&#xe3;o"/>
+<node CREATED="1191434887548" ID="Freemind_Link_1971912852" MODIFIED="1191435502734" TEXT="acelera&#xe7;&#xe3;o"/>
+<node CREATED="1191488086535" ID="Freemind_Link_1074069980" MODIFIED="1191488094919" TEXT="transi&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1191431801396" ID="Freemind_Link_1535268225" MODIFIED="1191486577049" POSITION="left" STYLE="bubble" TEXT="teoria">
+<edge COLOR="#ed2727" WIDTH="2"/>
+<node CREATED="1191447619900" ID="Freemind_Link_1924465917" MODIFIED="1191447639129" TEXT="estruturas internas aprendentes"/>
+<node CREATED="1191447641051" ID="Freemind_Link_1145249601" MODIFIED="1191447654721" TEXT="equivalentes funcionais"/>
+<node CREATED="1191489053624" ID="Freemind_Link_768273553" MODIFIED="1191489079479" TEXT="mapeamento mental">
+<node CREATED="1191489087208" ID="Freemind_Link_424655105" MODIFIED="1191489920970" TEXT="grandes &#xe1;reas">
+<edge COLOR="#18673c"/>
+</node>
+<node CREATED="1191489102271" ID="Freemind_Link_145228430" MODIFIED="1191489928380" TEXT="canais morfogen&#xe9;ticos">
+<edge COLOR="#18673c" WIDTH="2"/>
+</node>
+<node CREATED="1191489141723" ID="Freemind_Link_1056496094" MODIFIED="1191489930036" TEXT="fun&#xe7;&#xf5;es">
+<edge COLOR="#18673c" WIDTH="2"/>
+</node>
+<node CREATED="1191489151299" ID="Freemind_Link_1216778950" MODIFIED="1191489931320" TEXT="n&#xed;vel semi&#xf3;tico">
+<edge COLOR="#18673c" WIDTH="2"/>
+</node>
+<node CREATED="1191489183872" ID="Freemind_Link_1790893474" MODIFIED="1191489933036" TEXT="complexidade">
+<edge COLOR="#18673c" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191447667219" ID="Freemind_Link_1684845172" MODIFIED="1191447678520" TEXT="fio condutor"/>
+<node CREATED="1191447679279" ID="Freemind_Link_1772634302" MODIFIED="1191447686943" TEXT="elabora&#xe7;&#xe3;o dirigida"/>
+<node CREATED="1191486357748" ID="Freemind_Link_269349370" MODIFIED="1191486374986" TEXT="sete fases"/>
+<node CREATED="1191488324837" ID="Freemind_Link_468616155" MODIFIED="1191488341354" TEXT="grada&#xe7;&#xe3;o semi&#xf3;tica"/>
+<node CREATED="1191489214806" ID="Freemind_Link_520195712" MODIFIED="1191489237807" TEXT="navega&#xe7;&#xe3;o reconstrutiva"/>
+</node>
+<node CREATED="1191431744169" ID="_" MODIFIED="1191448031632" POSITION="right" STYLE="bubble" TEXT="hist&#xf3;rico">
+<edge COLOR="#43b11f"/>
+<node CREATED="1191432507905" ID="Freemind_Link_479604887" MODIFIED="1191432657912" TEXT="meninos lobo"/>
+<node CREATED="1191432658725" ID="Freemind_Link_1255537500" MODIFIED="1191432682868" TEXT="cogni&#xe7;&#xe3;o pela arte"/>
+<node CREATED="1191432683526" ID="Freemind_Link_1701122327" MODIFIED="1191432692242" TEXT="jogos de madeira"/>
+<node CREATED="1191432692931" ID="Freemind_Link_189734609" MODIFIED="1191432715534" TEXT="abrapa"/>
+<node CREATED="1191432912735" ID="Freemind_Link_609609733" MODIFIED="1191432927303" TEXT="ONU-UNIC"/>
+<node CREATED="1191432928045" ID="Freemind_Link_1956651789" MODIFIED="1191433027692" TEXT="NCE-UFRJ"/>
+</node>
+<node COLOR="#000000" CREATED="1191431821349" ID="Freemind_Link_1555083035" MODIFIED="1191486555667" POSITION="right" STYLE="bubble" TEXT="desdobramento">
+<edge COLOR="#1a03f1" WIDTH="2"/>
+<node CREATED="1191431845979" ID="Freemind_Link_1658990339" MODIFIED="1191489714427" TEXT="neurolab">
+<node CREATED="1191432396426" ID="Freemind_Link_75380336" MODIFIED="1191489761279" TEXT="transcri&#xe7;&#xe3;o de testes">
+<edge COLOR="#cc03f1"/>
+</node>
+<node CREATED="1191432420767" ID="Freemind_Link_1659263131" MODIFIED="1191489774270" TEXT="jogos neuropedag&#xf3;gicos">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191432441409" ID="Freemind_Link_1287201427" MODIFIED="1191489776502" TEXT="interdisciplinaridade">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191432992544" ID="Freemind_Link_784858085" MODIFIED="1191489778010" TEXT="atendimento">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191431854579" ID="Freemind_Link_1068352677" MODIFIED="1191431858298" TEXT="labase">
+<node CREATED="1191432157605" ID="Freemind_Link_743571097" MODIFIED="1191489780750" TEXT="sistemas de informa&#xe7;&#xe3;o">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+<node CREATED="1191432268931" ID="Freemind_Link_1776873922" MODIFIED="1191489841089" TEXT="plataformas">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191432182102" ID="Freemind_Link_1685513507" MODIFIED="1191489782694" TEXT="sistemas educacionais">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+<node CREATED="1191432199839" ID="Freemind_Link_886882818" MODIFIED="1191489842205" TEXT="pindorama">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191432225930" ID="Freemind_Link_16855954" MODIFIED="1191489784570" TEXT="ferramentas">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+<node CREATED="1191432252970" ID="Freemind_Link_1404921713" MODIFIED="1191489837761" TEXT="phidias">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+<node CREATED="1191432953790" ID="Freemind_Link_1007865681" MODIFIED="1191489838945" TEXT="greenfoot">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+</node>
+</node>
+<node CREATED="1191431859539" ID="Freemind_Link_559306146" MODIFIED="1191431881600" TEXT="neurolabase">
+<node CREATED="1191432342975" ID="Freemind_Link_216224135" MODIFIED="1191489787310" TEXT="kuarup">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191432350163" ID="Freemind_Link_958330049" MODIFIED="1191489788546" TEXT="telemedicina">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191432385081" ID="Freemind_Link_1882378214" MODIFIED="1191489790178" TEXT="plataformas">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+<node CREATED="1191489381880" ID="Freemind_Link_1297780667" MODIFIED="1191489821976" TEXT="telemedicina">
+<edge COLOR="#f17b03"/>
+</node>
+<node CREATED="1191489404264" ID="Freemind_Link_1074835171" MODIFIED="1191489829741" TEXT="teleeduca&#xe7;&#xe3;o">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+<node CREATED="1191489422464" ID="Freemind_Link_616229780" MODIFIED="1191489830989" TEXT="pesquisa">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+<node CREATED="1191489445439" ID="Freemind_Link_903345811" MODIFIED="1191489832361" TEXT="servi&#xe7;o">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+<node CREATED="1191489452599" ID="Freemind_Link_213096745" MODIFIED="1191489833701" TEXT="cidadania">
+<edge COLOR="#f17b03" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191489343669" ID="Freemind_Link_1634060620" MODIFIED="1191489792346" TEXT="salas">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191431882525" ID="Freemind_Link_1646061457" MODIFIED="1191432891877" TEXT="cursos">
+<node CREATED="1191431893001" ID="Freemind_Link_571750065" MODIFIED="1191489793506" TEXT="PPGI-IM-NCE">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191431922911" ID="Freemind_Link_1879253769" MODIFIED="1191489794786" TEXT="neuropedagogia">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191432824526" ID="Freemind_Link_1347639414" MODIFIED="1191489795926" TEXT="profissionalizante">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191432856645" ID="Freemind_Link_1891124736" MODIFIED="1191489797106" TEXT="multiplicadores">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191486412103" ID="Freemind_Link_763638140" MODIFIED="1191486429362" TEXT="opera&#xe7;&#xe3;o">
+<node CREATED="1191486434515" ID="Freemind_Link_1859827583" MODIFIED="1191489798242" TEXT="atendimento">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191488811947" ID="Freemind_Link_598278912" MODIFIED="1191489799966" TEXT="servi&#xe7;os">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191486446639" ID="Freemind_Link_863022322" MODIFIED="1191489801654" TEXT="pesquisa">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191488830287" ID="Freemind_Link_1025896785" MODIFIED="1191489802826" TEXT="difus&#xe3;o">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+<node CREATED="1191488847466" ID="Freemind_Link_372566359" MODIFIED="1191489804098" TEXT="cidadania">
+<edge COLOR="#cc03f1" WIDTH="2"/>
+</node>
+</node>
+</node>
+</node>
+</map>

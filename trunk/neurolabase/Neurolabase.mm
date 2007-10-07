0b7c60905cc466f74fe1a8e2171ea54708372039
cetoli <cetoli@87632e9f-ef39-0410-bef6-35b1ae510437>
2007-10-07 23:59:42 +0000

    Curso de neuropedagogia  da Carla - versão preliminar

---
+++ trunk/neurolabase/Neurolabase.mm
@@ -0,0 +1,322 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1191704177143" ID="Freemind_Link_1225745659" MODIFIED="1191761360272" TEXT="Neuropedagogia&#xa;Inform&#xe1;tica">
+<edge STYLE="sharp_bezier" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1191750953212" FOLDED="true" ID="Freemind_Link_188857762" MODIFIED="1191760739791" POSITION="left" STYLE="bubble" TEXT="projetos">
+<edge COLOR="#013483" STYLE="sharp_bezier" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1191704202968" ID="Freemind_Link_136335274" MODIFIED="1191760739787" TEXT="Neurolab">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704342938" ID="Freemind_Link_1249812763" MODIFIED="1191760739782" TEXT="INES">
+<edge COLOR="#630db5"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704948047" ID="Freemind_Link_1499177852" MODIFIED="1191760739780" TEXT="testes - surdos">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704998099" ID="Freemind_Link_4784396" MODIFIED="1191760739778" TEXT="releases cient&#xed;ficas">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191705041774" ID="Freemind_Link_1324114930" MODIFIED="1191760739777" TEXT="computa&#xe7;&#xe3;o qualitativa">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+</node>
+<node CREATED="1191704358442" ID="Freemind_Link_1164679081" MODIFIED="1191760739775" TEXT="IBC">
+<edge COLOR="#630db5" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191705095473" ID="Freemind_Link_1288514089" MODIFIED="1191760739772" TEXT="testes - invidentes">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191705125421" ID="Freemind_Link_1827500985" MODIFIED="1191760739770" TEXT="jogos audio-mentais">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+</node>
+</node>
+<node CREATED="1191751545161" ID="Freemind_Link_1875574728" MODIFIED="1191760739768" TEXT="neurolabase">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704372142" ID="Freemind_Link_1015682561" MODIFIED="1191760739765" TEXT="plataformas">
+<edge COLOR="#630db5" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704417625" ID="Freemind_Link_1835386965" MODIFIED="1191760739762" TEXT="telemedicina">
+<edge COLOR="#7f1a76"/>
+</node>
+<node CREATED="1191704426381" ID="Freemind_Link_1299038534" MODIFIED="1191760739761" TEXT="pesquisa">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704439113" ID="Freemind_Link_1147935265" MODIFIED="1191760739759" TEXT="servi&#xe7;o">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704449637" ID="Freemind_Link_1101909191" MODIFIED="1191760739758" TEXT="educa&#xe7;&#xe3;o">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704455229" ID="Freemind_Link_31629427" MODIFIED="1191760739757" TEXT="cidadania">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+</node>
+<node CREATED="1191751608316" ID="Freemind_Link_119419588" MODIFIED="1191760739755" TEXT="projetos">
+<edge COLOR="#630db5" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751645920" ID="Freemind_Link_763265943" MODIFIED="1191760739752" TEXT="telemedicina progn&#xf3;stica">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191751677860" ID="Freemind_Link_310303876" MODIFIED="1191760739751" TEXT="neuropedagogia inform&#xe1;tica">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191751715616" ID="Freemind_Link_611621824" MODIFIED="1191760739749" TEXT="educa&#xe7;&#xe3;o neuropedag&#xf3;gica">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+</node>
+</node>
+<node CREATED="1191704208252" ID="Freemind_Link_279936342" MODIFIED="1191760739748" TEXT="Labase">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704586080" ID="Freemind_Link_1446097599" MODIFIED="1191760739745" TEXT="sistemas">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704849272" ID="Freemind_Link_841107055" MODIFIED="1191760739742" TEXT="web 2.0">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704874104" ID="Freemind_Link_1765467150" MODIFIED="1191760739741" TEXT="bayesianos">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+</node>
+<node CREATED="1191704639047" ID="Freemind_Link_295213395" MODIFIED="1191760739739" TEXT="grids">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704715070" ID="Freemind_Link_461164095" MODIFIED="1191760739736" TEXT="atlas">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704817277" ID="Freemind_Link_461366444" MODIFIED="1191760739735" TEXT="hesperides">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+</node>
+<node CREATED="1191704643979" ID="Freemind_Link_1431524503" MODIFIED="1191760739734" TEXT="educa&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704659847" ID="Freemind_Link_351787221" MODIFIED="1191760739730" TEXT="pindorama">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704668251" ID="Freemind_Link_1074892368" MODIFIED="1191760739728" TEXT="phidias">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704682298" ID="Freemind_Link_1826522659" MODIFIED="1191760739726" TEXT="acadia">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+<node CREATED="1191704789349" ID="Freemind_Link_1012457795" MODIFIED="1191760739725" TEXT="kuarup">
+<edge COLOR="#7f1a76" WIDTH="4"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1191750977840" ID="Freemind_Link_1560558848" MODIFIED="1191760739723" POSITION="left" STYLE="bubble" TEXT="atividades">
+<edge COLOR="#107054" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node CREATED="1191752217629" ID="Freemind_Link_316945183" MODIFIED="1191760739719" TEXT="palestras">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191755010037" ID="Freemind_Link_183268343" MODIFIED="1191760739716" TEXT="introdu&#xe7;&#xe3;o">
+<edge COLOR="#567f02"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191756204535" ID="Freemind_Link_148917690" MODIFIED="1191760739713" TEXT="neuropedagia inform&#xe1;tica">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191752224969" ID="Freemind_Link_1843170386" MODIFIED="1191760739710" TEXT="cursos">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191755055548" ID="Freemind_Link_739556030" MODIFIED="1191760739707" TEXT="programa&#xe7;&#xe3;o e jogos">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755078504" ID="Freemind_Link_1067611314" MODIFIED="1191760739704" TEXT="neuropedagogia inform&#xe1;tica">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755114031" ID="Freemind_Link_538727943" MODIFIED="1191760739701" TEXT="psiquiatria e abandono">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191756233722" ID="Freemind_Link_1654344832" MODIFIED="1191760739698" TEXT="propostas de p&#xf3;s-gradua&#xe7;&#xf5;es">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191752234129" ID="Freemind_Link_858848997" MODIFIED="1191760739695" TEXT="pesquisa">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191755169467" ID="Freemind_Link_1099347281" MODIFIED="1191760739692" TEXT="constru&#xe7;&#xe3;o de jogos e ambientes">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755217130" ID="Freemind_Link_1061872956" MODIFIED="1191760739689" TEXT="prataformas web">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755253898" ID="Freemind_Link_1048273673" MODIFIED="1191760739685" TEXT="avalia&#xe7;&#xe3;o interdiciplinar de indiv&#xed;duos">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755298157" ID="Freemind_Link_1356142660" MODIFIED="1191760739682" TEXT="redes de conhecimento">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191752244089" ID="Freemind_Link_696478656" MODIFIED="1191760739679" TEXT="orienta&#xe7;&#xf5;es">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191755363820" ID="Freemind_Link_1502663728" MODIFIED="1191760739675" TEXT="produ&#xe7;&#xe3;o e diversidade textual">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755423356" ID="Freemind_Link_263471581" MODIFIED="1191760739672" TEXT="ambientes e jogos neuropedag&#xf3;gicos">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755558826" ID="Freemind_Link_1776061833" MODIFIED="1191760739670" TEXT="redes sociais de pesquisa e educa&#xe7;&#xe3;o">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755643610" ID="Freemind_Link_330842783" MODIFIED="1191760739667" TEXT="adapta&#xe7;&#xe3;o de testes psicol&#xf3;gicos">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755712772" ID="Freemind_Link_624850103" MODIFIED="1191760739664" TEXT="assimila&#xe7;&#xe3;o computacional de protocolos">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191755976409" ID="Freemind_Link_535304689" MODIFIED="1191760739661" TEXT="tratamento inteligente de dados">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191756052768" ID="Freemind_Link_609334854" MODIFIED="1191760739658" TEXT="arcabou&#xe7;os de software neuropedag&#xf3;gico">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191756101152" ID="Freemind_Link_1569454409" MODIFIED="1191760739655" TEXT="design de plataformas e ambientes neuropedag&#xf3;gicos">
+<edge COLOR="#567f02" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1191750941612" ID="Freemind_Link_412928718" MODIFIED="1191760739650" POSITION="right" STYLE="bubble" TEXT="institui&#xe7;&#xf5;es">
+<edge COLOR="#f11a1a" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1191704189866" ID="_" MODIFIED="1191760739647" TEXT="Abrapa">
+<edge COLOR="#f11a1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191704269011" ID="Freemind_Link_441240818" MODIFIED="1191760739644" TEXT="Jogos cognitivos">
+<edge COLOR="#f16f1a"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191704286287" ID="Freemind_Link_1734601539" MODIFIED="1191760739639" TEXT="atendimento psicol&#xf3;gico">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191704313563" ID="Freemind_Link_11113074" MODIFIED="1191760739636" TEXT="crian&#xe7;as abusadas">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191704221484" ID="Freemind_Link_952258170" MODIFIED="1191760739634" TEXT="LABS">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751443035" ID="Freemind_Link_1012269134" MODIFIED="1191760739631" TEXT="instituto de neurologia">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191751462114" ID="Freemind_Link_841208740" MODIFIED="1191760739628" TEXT="telemedicina">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191751481950" ID="Freemind_Link_878268600" MODIFIED="1191760739625" TEXT="compet&#xea;ncia moral">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191751368011" ID="Freemind_Link_922551095" MODIFIED="1191760739622" TEXT="UFRJ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751199805" ID="Freemind_Link_1687270404" MODIFIED="1191760739619" TEXT="NCE">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751221769" ID="Freemind_Link_881343943" MODIFIED="1191760739616" TEXT="PPGI-IM">
+<edge COLOR="#997a1e"/>
+</node>
+<node CREATED="1191751287645" ID="Freemind_Link_1893505008" MODIFIED="1191761365038" TEXT="projetos de pesquisa">
+<edge COLOR="#997a1e" WIDTH="2"/>
+</node>
+<node CREATED="1191751296948" ID="Freemind_Link_1638382761" MODIFIED="1191760739613" TEXT="mestrado">
+<edge COLOR="#997a1e" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191704233200" ID="Freemind_Link_1067640040" MODIFIED="1191760739611" TEXT="INDC">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191752041875" ID="Freemind_Link_961589625" MODIFIED="1191760739609" TEXT="pacientes neurol&#xf3;gicos">
+<edge COLOR="#997a1e" WIDTH="2"/>
+</node>
+<node CREATED="1191756185451" ID="Freemind_Link_432367416" MODIFIED="1191760739607" TEXT="telemedicina">
+<edge COLOR="#997a1e" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1191751397403" ID="Freemind_Link_1614094010" MODIFIED="1191760739606" TEXT="fonoaudiologia">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751851730" ID="Freemind_Link_217613152" MODIFIED="1191760739603" TEXT="gradua&#xe7;&#xe3;o">
+<edge COLOR="#997a1e" WIDTH="2"/>
+</node>
+<node CREATED="1191751864945" ID="Freemind_Link_1440771680" MODIFIED="1191760739601" TEXT="TUIA">
+<edge COLOR="#997a1e" WIDTH="2"/>
+</node>
+</node>
+</node>
+<node CREATED="1191704239059" ID="Freemind_Link_1280220767" MODIFIED="1191760739598" TEXT="PUC">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751319712" ID="Freemind_Link_564820956" MODIFIED="1191760739595" TEXT="psicologia">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191751336796" ID="Freemind_Link_1924920069" MODIFIED="1191760739592" TEXT="educa&#xe7;&#xe3;o">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191751345984" ID="Freemind_Link_1902590954" MODIFIED="1191760739589" TEXT="desenho industrial">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191751115210" ID="Freemind_Link_994874654" MODIFIED="1191760739586" TEXT="ONU">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751130446" ID="Freemind_Link_664310139" MODIFIED="1191760739583" TEXT="GT direitos humanos">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191751156782" ID="Freemind_Link_1738159039" MODIFIED="1191760739580" TEXT="Juventude pela paz">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191752105535" ID="Freemind_Link_235110177" MODIFIED="1191760739577" TEXT="chancela abrapa">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191751896913" ID="Freemind_Link_1819294525" MODIFIED="1191760739574" TEXT="Secretaria municipal">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191752021540" ID="Freemind_Link_1217515105" MODIFIED="1191760739571" TEXT="ensino fundamental">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191752077691" ID="Freemind_Link_285555645" MODIFIED="1191760739568" TEXT="rede municipal">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1191751910749" ID="Freemind_Link_909639835" MODIFIED="1191760739565" TEXT="secretaria estadual">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1191751939673" ID="Freemind_Link_791673275" MODIFIED="1191760739562" TEXT="NTE">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1191751984712" ID="Freemind_Link_1653326016" MODIFIED="1191760739559" TEXT="educa&#xe7;&#xe3;o especial">
+<edge COLOR="#f16f1a" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ trunk/neurolabase/neurolabase.rst
@@ -0,0 +1,170 @@
+=======================================================
+ Neuropedagogia Informática
+=======================================================
+
+.. footer:: LABASE - NEUROLAB
+
+.. class:: right big
+
+   | *Carlo Emmanoel Tolla de Oliveira*
+   | carlo@nce.ufrj.br
+   | `LABASE <http://labase.nce.ufrj.br>`_
+   | *Carla Verônica Machado Marques*
+   | abrapacarla@gmail.com
+   | `ABRAPA-BR <http://www.abrapabr.org.br>`_
+
+.. class:: small
+
+   ©2007, licensed under a `Creative Commons
+   Attribution/Share-Alike (BY-SA) license
+   <http://creativecommons.org/licenses/by-sa/3.0/>`__.
+
+Agenda
+--------------------------------------------------------------------------------
+
+* De Lobos ao Reverendo
+* Introdução à Neuropedagogia Informática
+* Interdiciplinaridade Psi-Info
+* O projeto de Pesquisa
+* Atividades do Grupo de pesquisa
+* Participações no Projeto
+
+De Lobos ao Reverendo
+--------------------------------------------------------------------------------
+
+* Alfabetização de meninos lobo
+* Atendimento de crianças abusadas
+* Abrapa - brinquedos neuropedagógicos
+* Abrigos da prefeitura
+* A vara de infância
+* A chancela da ONU
+* A parceria com o NCE
+
+A Neuropedagogia Informática
+--------------------------------------------------------------------------------
+
+* `Mapa Conceitual da Neuropedagogia Informática <http://neuropedagogia.pbwiki.com/ProjetoNeuropedagogiaInfo>`_
+* Obtendo e estudando o perfil de pacientes
+* Construindo jogos neuropedagógicos
+* Plataformas para caracterização e atendimento
+* Plataformas para formação de redes de conhecimento
+
+
+Interdiciplinaridade Psi-Info
+--------------------------------------------------------------------------------
+
+**Psicologia, Fonoaudiologia**
+  Avaliação e atendimento
+**Neurologia, Psiquiatria,**
+  Ententimento do cérebro e psiquê
+**Letras, Música**
+  Processo formal de comunicação
+**Artes, Design**
+  Processo artístico de comunicação
+**Informática, Computação**
+  Processo eletrônico de comunicação
+
+O projeto de Pesquisa
+--------------------------------------------------------------------------------
+
+**Projeto Neurolab**
+   Psicologia no atentimento a deficientes e abusados
+ 
+**Projeto Labase**
+   Sistemas de informação, Grids e plataformas Educacionais
+ 
+**Projeto Neurolabase**
+ Plataformas neuropedagógicas, científicas, educacionais, humanitárias
+
+Plataformas Neurolabase
+--------------------------------------------------------------------------------
+
+**Plataformas neuropedagógicas**
+   `TUIA <http://www.abrapabr.org.br/plataformas/tuia/index.html>`_, `INES <http://www.abrapabr.org.br/plataformas/ines/index.html>`_, `IBC <http://www.abrapabr.org.br/plataformas/ibc/index.html>`_, `Cristaal <http://www.abrapabr.org.br/plataformas/cristaal/index.html>`_
+**Plataformas científicas**
+   `Neurolab-INES <http://www.abrapabr.org.br/plataformas/neurolab/index.html>`_, `Neurolab-IBC <http://www.abrapabr.org.br/plataformas/neurolab/index.html>`_, `Ouro <http://www.abrapabr.org.br/plataformas/ouro/index.html>`_
+**Plataformas educacionais**
+ `Esgrima <http://www.abrapabr.org.br/plataformas/esgrima/index.html>`_
+**Plataformas educacionais**
+ `Abrapa <http://www.abrapabr.org.br/plataformas/abrapa/index.html>`_, `Paz <http://www.abrapabr.org.br/plataformas/paz/index.html>`_
+
+
+Atividades do Grupo de Pesquisa
+--------------------------------------------------------------------------------
+
+**Palestras**
+   * Introdução à Neuropedagogia
+   * Neuropedagogia e Informática
+ 
+**Cursos**
+   Programação, Psiquiatria e Neuropedagogia
+ 
+**Pesquisa**
+   Plataformas neuropedagógicas, científicas, educacionais, humanitárias
+
+
+Cursos do Grupo de Pesquisa
+--------------------------------------------------------------------------------
+
+**Programação e Jogos Neuropedagógicos**
+   * `Desenvolvimento de Jogos <http://labase.nce.ufrj.br/cursos/poo>`_, `Projeto Kuarup <http://kuarup.pbwiki.com>`_
+**Fonoaudiologia e Psiquiatria**
+   * Abandono, negligência e abuso
+   * Produção científica da plataforma `TUIA-NCE <http://abrapa.nce.ufrj.br>`_
+**Neuropedagogia Informática**
+   * `Fundamentos científicos, modelos e paradigmas <http://labase.nce.ufrj.br/cursos/neuropedagogia>`_
+   * Plataformas, jogos e ambientes neuropedagógicos
+
+
+Projetos de Pesquisa
+--------------------------------------------------------------------------------
+
+**Programação de Jogos Neuropedagógicos**
+   * `Desenvolvimento de Jogos - Pindorama <http://pindorama.nce.ufrj.br>`_
+   * `Projeto Kuarup - Vygotsky <http://kuarup.pbwiki.com/PotiguarAcau%C3%A3>`_
+**Avaliação Psicológica**
+   * Transcrição de testes para Português e Libras
+   * Assimilação computacional de protocolos
+**Plataformas Neuropedagógicas**
+   * Acesso e difusão da neuropedagogia
+
+   
+
+Plataformas Neuropedagógicas
+--------------------------------------------------------------------------------
+
+* `Projeto de plataformas WEB 2.0  <http://code.google.com/p/tuia/>`_
+* Processamento inteligente por lógica bayesiana
+* Armazenamento  para acompanhamento integrado
+* Prognósticos da condição mental e neurocognitiva
+* Acesso a tratamento com jogos neuropedagógicos
+* Laboratórios para experimentação neuroconitiva
+* Ciência e cidadania por redes de conhecimento
+
+Diagrama das Plataformas
+--------------------------------------------------------------------------------
+.. image:: http://www.best4c.com/Best4cUserFiles/20071008/12851_1191797713906.jpg
+
+Participações no Projeto
+--------------------------------------------------------------------------------
+
+**ONGs**
+   ABRAPA, ONU
+**Universidade**
+   UFRJ, PUC, UERJ
+**Governo e Autarquias**
+   Secretarias de Educação Estadual e Municipal, Fiocruz
+**Setor Privado**
+   LABS
+
+Conclusão
+--------------------------------------------------------------------------------
+
+* Encontro entre a psicologia, neurociência e informática
+* Participação de vários setores da sociedade
+* A educação com respaldo científico
+* Produção de ciência e tecnologia de acesso
+* Acessibilidade a uma melhor educação e tratamento
+* Perspectivas de melhoria de qualidade de vida
+
+
